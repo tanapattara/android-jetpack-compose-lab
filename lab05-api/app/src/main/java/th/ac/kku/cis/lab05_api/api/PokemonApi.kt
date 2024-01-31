b9da8d4daa3a77544098edc35b60fb428f6c3ad5
Tanapattara Wongkhamchan <tanapattara.w@gmail.com>
2024-01-31 16:28:03 +0700

    add new api id

--- lab05-api/app/src/main/java/th/ac/kku/cis/lab05_api/api/PokemonApi.kt
+++ lab05-api/app/src/main/java/th/ac/kku/cis/lab05_api/api/PokemonApi.kt
@@ -3,6 +3,7 @@ package th.ac.kku.cis.lab05_api.api
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Path
 import th.ac.kku.cis.lab05_api.model.PokemonList
 
 interface PokemonApi {
@@ -11,4 +12,6 @@ interface PokemonApi {
     )
     @GET("pokemon")
     abstract fun getPokemonList() : Call<PokemonList>
+    @GET("pokemon/{id}")
+    abstract fun getPokemonDetail(@Path("id") id:String)
 }
